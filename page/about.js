dad914ba79f07e1baf605892ab00f3c0186682c7
ZETA-A <open120477@gmail.com>
2026-05-19 02:01:50 +0900

    📝 프로필 페이지 카드 추가

--- page/about.js
+++ page/about.js
@@ -1,13 +1,20 @@
-const about = () => /*html*/`
+const about = () => /*html*/ `
     <section class="hero-section">
-        <div class="capsule">
-            <div class="profile-image-wrapper">
-                <img src="./assets/profile.png" alt="Profile Image" class="profile-image"/>
+        <div class="capsule profile-card">
+            <img src="./assets/profile.png" alt="Profile Image" class="profile-image"/>
+            <div class="profile-content">
+                <blockquote class="profile-quote">
+                    <p class="profile-description">${langData.profile.blockquoteDescription}</p>
+                </blockquote>
+                <p class="profile-name">${langData.profile.blockquoteCite}</p>
             </div>
         </div>
         <div class="capsule">
-            <h1 class="profile-name">${langData.profile.name}</h1>
-            <p class="profile-description">저는 풀스택 개발자 김승현 입니다!</p>
+
         </div>
+        <div class="capsule">
+
+        </div>
+        
     </section>
 `;
